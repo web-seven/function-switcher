15e2b08d4532fb5c0f95d94605ce92940cf6ddbd
art10m <artiomberdeu@gmail.com>
2024-03-06 11:57:29 +0200

    Fixed linting

--- fn.go
+++ fn.go
@@ -9,6 +9,7 @@ import (
 	"github.com/crossplane/function-sdk-go/logging"
 	fnv1beta1 "github.com/crossplane/function-sdk-go/proto/v1beta1"
 	"github.com/crossplane/function-sdk-go/request"
+	"github.com/crossplane/function-sdk-go/resource"
 	"github.com/crossplane/function-sdk-go/response"
 	"github.com/pkg/errors"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -25,18 +26,43 @@ func (f *Function) RunFunction(_ context.Context, req *fnv1beta1.RunFunctionRequ
 
 	rsp := response.To(req, response.DefaultTTL)
 
-	oxr, err := request.GetObservedCompositeResource(req)
+	desired, err := request.GetDesiredComposedResources(req)
 	if err != nil {
-		response.Fatal(rsp, errors.Wrapf(err, "cannot get observed composite resource from %T", req))
+		response.Fatal(rsp, err)
 		return rsp, nil
 	}
 
+	switchOn, switchOff, err := collectSwitches(req, rsp)
+	if err != nil {
+		return rsp, nil
+	}
+
+	filterDesired(desired, switchOff, switchOn)
+
+	rsp.Desired.Resources = nil
+
+	if err := response.SetDesiredComposedResources(rsp, desired); err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot set desired composed resources in %T", rsp))
+		return rsp, nil
+	}
+
+	return rsp, nil
+}
+
+func collectSwitches(req *fnv1beta1.RunFunctionRequest, rsp *fnv1beta1.RunFunctionResponse) ([]string, []string, error) {
+	oxr, err := request.GetObservedCompositeResource(req)
+	if err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot get observed composite resource from %T", req))
+		return nil, nil, err
+	}
 	var switchOn []string
 	var switchOff []string
-	meta := v1.ObjectMeta{}
 	m := oxr.Resource.Object["metadata"]
-	mm, _ := json.Marshal(m)
-	json.Unmarshal(mm, &meta)
+
+	meta, err := toMeta(m.(map[string]interface{}))
+	if err != nil {
+		return nil, nil, err
+	}
 
 	for k, v := range meta.Annotations {
 		if strings.Contains(k, "switcher.fn.kndp.io/enabled") {
@@ -54,11 +80,10 @@ func (f *Function) RunFunction(_ context.Context, req *fnv1beta1.RunFunctionRequ
 		}
 	}
 
-	desired, err := request.GetDesiredComposedResources(req)
-	if err != nil {
-		response.Fatal(rsp, err)
-		return rsp, nil
-	}
+	return switchOn, switchOff, nil
+}
+
+func filterDesired(desired map[resource.Name]*resource.DesiredComposed, switchOff []string, switchOn []string) map[resource.Name]*resource.DesiredComposed {
 
 	for r := range desired {
 		if switchOff != nil && slices.Contains[[]string, string](switchOff, string(r)) {
@@ -69,12 +94,20 @@ func (f *Function) RunFunction(_ context.Context, req *fnv1beta1.RunFunctionRequ
 		}
 	}
 
-	rsp.Desired.Resources = nil
+	return desired
+}
 
-	if err := response.SetDesiredComposedResources(rsp, desired); err != nil {
-		response.Fatal(rsp, errors.Wrapf(err, "cannot set desired composed resources in %T", rsp))
-		return rsp, nil
+func toMeta(m map[string]interface{}) (v1.ObjectMeta, error) {
+	meta := v1.ObjectMeta{}
+
+	mm, err := json.Marshal(m)
+	if err != nil {
+		return meta, err
+	}
+	err = json.Unmarshal(mm, &meta)
+	if err != nil {
+		return meta, err
 	}
 
-	return rsp, nil
+	return meta, nil
 }
